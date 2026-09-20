79c9df078ce497f466125d2caf449923184e49df
Chopapik <krzysztof.babik72@gmail.com>
2026-09-20 14:03:43 +0200

    fix(frontend): bundle MapLibre worker for Docker

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './config/maplibre'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import App from './App'
 import './index.css'
